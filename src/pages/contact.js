3d4ea83ebdee5b5ba2c6ab2ddd29061f515fbb8f
kameron johnson <kameronjohnson@gmail.com>
2018-02-13 22:47:40 -0800

    revert link styles

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,7 @@ const ContactPage = () => (
   <div className="inside contact">
     <div className="inside-text">
       <p>Feel free to contact me at the email below. <br />I'd love to chat!</p>
-      <p><a className="contact-link" href="mailto:kameronjohnson@gmail.com">KameronJohnson@gmail.com</a></p>
+      <p><a href="mailto:kameronjohnson@gmail.com">KameronJohnson@gmail.com</a></p>
     </div>
   </div>
 )
