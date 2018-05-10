9d4f1752ada4507e3c9c52496c5ab6eb6a2f21fa
kameron johnson <kameronjohnson@gmail.com>
2018-05-09 17:54:18 -0700

    fix deployment bugs

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,42 +6,42 @@
 
  // You can delete this file if you're not using it
 
- const path = require('path');
+//  const path = require('path');
 
- //createPages from the api
- exports.createPages = ({boundActionCreators, graphql}) => {
-     const {createPage} = boundActionCreators;
+//  //createPages from the api
+//  exports.createPages = ({boundActionCreators, graphql}) => {
+//      const {createPage} = boundActionCreators;
      
-     const postTemplate = path.resolve('src/templates/post.js');
+//      const postTemplate = path.resolve('src/templates/post.js');
 
-     //run query via graphql to get all markdown files
-     return graphql(`{
-         allMarkdownRemark {
-             edges {
-                 node {
-                     html
-                     id
-                     frontmatter {
-                         path
-                         title
-                         date
-                     }
-                 }
-             }
-         }
-     }`)
-     .then(res => {
-         if(res.errors) {
-             return Promise.reject(res.errors);
-         }
+//      //run query via graphql to get all markdown files
+//      return graphql(`{
+//          allMarkdownRemark {
+//              edges {
+//                  node {
+//                      html
+//                      id
+//                      frontmatter {
+//                          path
+//                          title
+//                          date
+//                      }
+//                  }
+//              }
+//          }
+//      }`)
+//      .then(res => {
+//          if(res.errors) {
+//              return Promise.reject(res.errors);
+//          }
 
-         //this is an array of all our pages
-         res.data.allMarkdownRemark.edges.forEach(({node}) => {
-            createPage({
-                path: node.frontmatter.path,
-                component: postTemplate
-            })
-         })
+//          //this is an array of all our pages
+//          res.data.allMarkdownRemark.edges.forEach(({node}) => {
+//             createPage({
+//                 path: node.frontmatter.path,
+//                 component: postTemplate
+//             })
+//          })
 
-     })
- }
+//      })
+//  }
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import Helmet from 'react-helmet';
+// import React from 'react';
+// import Helmet from 'react-helmet';
 
-export default function Template({data}) {
-    // const post = data.markdownRemark;
-    const {markdownRemark: post} = data;
+// export default function Template({data}) {
+//     // const post = data.markdownRemark;
+//     const {markdownRemark: post} = data;
 
-    return (
-        <div>
-            <h1>{post.frontmatter.title}</h1>
-            <div dangerouslySetInnerHTML={{__html: post.html}} />
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             <h1>{post.frontmatter.title}</h1>
+//             <div dangerouslySetInnerHTML={{__html: post.html}} />
+//         </div>
+//     )
+// }
 
-export const postQuery = graphql`
-    query BlogPostByPath($path: String!){
-        markdownRemark(frontmatter: { path: { eq: $path} }) {
-            html
-            frontmatter {
-                path
-                title
-            }
-        }
-    }
-`
+// export const postQuery = graphql`
+//     query BlogPostByPath($path: String!){
+//         markdownRemark(frontmatter: { path: { eq: $path} }) {
+//             html
+//             frontmatter {
+//                 path
+//                 title
+//             }
+//         }
+//     }
+// `
