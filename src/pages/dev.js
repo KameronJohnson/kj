c888a9d9393c09ef00c15ad4fa1d61cfea322de6
Kameron Johnson <kjohnson@parthenonsoftware.com>
2019-11-26 15:32:35 -0800

    remove old dev projects, add overlay

--- src/pages/dev.js
+++ src/pages/dev.js
@@ -4,22 +4,23 @@ import KJResume from "../docs/KJ-Resume-2019.pdf";
 import FaGithub from 'react-icons/lib/fa/github';
 
 const DevPage = () => (
-  <div className="inside dev">
-    <div className="flex-container-dev">
-      <div className="flex-intro-dev text-dev">
-        <p>
-        I enjoy creating excellent modern websites and applications while keeping up with the latest JavaScript frameworks and web technologies. I currently work as a Front-End Developer at <a href="http://parthenonsoftware.com">Parthenon Software Group</a>, a software company with over 10 years of expertise providing custom software solutions.
+	<div className="inside dev">
+		<div className="flex-container-dev">
+			<div className="flex-intro-dev text-dev">
+				<p>
+					I enjoy creating excellent modern websites and applications while keeping up with the latest JavaScript frameworks and web technologies. I currently work as a Front-End Developer at <a href="http://parthenonsoftware.com">Parthenon Software Group</a>, a software company with over 10 years of expertise providing custom software solutions.
         </p>
-        <p>
-        
-        </p>
-        <p>I've worked on a variety of greenfield and brownfield development projects for clients including <a href="https://starterpack.intel.com/offer/" target="_blank">Intel</a>, <a href="https://blueprintlsat.com" target="_blank">Blueprint LSAT</a>, <a href="https://www.infocus.com/" target="_blank">InFocus</a>, <a href="https://www.authentic8.com/" target="_blank">Authentic8</a>, <a href="https://www.integritylogistics.com/" target="_blank">Integrity Logistics</a>, <a href="https://buynowcc.com/" target="_blank">BuyNow</a> & <a href="https://www.pooltables.com/" target="_blank">Pooltables.com</a>.</p>
-        <p>Feel free to take a gander at my <a href={KJResume} target="_blank">Resume</a>.</p>
-        <hr></hr>
-        <p className="projects">Below are some older personal projects to improve my skills.<br /> Codebases accessible via my <a href="http://github.com/kameronjohnson" target="_blank">Github</a>.</p>
-      </div>
+				<p>
 
-      <div className="flex-dev">
+				</p>
+				<p>I've worked on a variety of greenfield and brownfield development projects for clients including <a href="https://starterpack.intel.com/offer/" target="_blank">Intel</a>, <a href="https://blueprintlsat.com" target="_blank">Blueprint LSAT</a>, <a href="https://www.infocus.com/" target="_blank">InFocus</a>, <a href="https://www.authentic8.com/" target="_blank">Authentic8</a>, <a href="https://www.integritylogistics.com/" target="_blank">Integrity Logistics</a>, <a href="https://buynowcc.com/" target="_blank">BuyNow</a> & <a href="https://www.pooltables.com/" target="_blank">Pooltables.com</a>.</p>
+				<p>Feel free to take a gander at my <a href={KJResume} target="_blank">Resume</a>.</p>
+				{/* <hr></hr>
+        <p className="projects">Below are some older personal projects to improve my skills.<br /> Codebases accessible via my <a href="http://github.com/kameronjohnson" target="_blank">Github</a>.</p>
+       */}
+			</div>
+
+			{/* <div className="flex-dev">
         <h3><a href="https://github.com/KameronJohnson/React-YouTube-Search" target="_blank">Youtube Search</a></h3>
         <p>A blazing fast Youtube video search app built with React. 
           <span>
@@ -83,9 +84,9 @@ const DevPage = () => (
         <a href="https://dog-fetch.netlify.com/" target="_blank">
           <img src={require("../images/dog-fetch.png")}/>
         </a>
-      </div>             
-    </div>
-  </div>
+      </div>              */}
+		</div>
+	</div>
 )
 
 export default DevPage
